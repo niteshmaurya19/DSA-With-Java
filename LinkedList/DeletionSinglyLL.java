616e94fb914f2e7591f69103d5f744e0ff87435e
Nitesh Maurya <niteshmauryaji02@gmail.com>
2026-09-01 23:20:49 +0530

    Delete at front

---
+++ LinkedList/DeletionSinglyLL.java
@@ -0,0 +1,67 @@
+package LinkedList;
+import java.util.Scanner;
+public class DeletionSinglyLL {
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    int n;
+    Node head;
+    public void CreateList(Scanner sc){
+
+        System.out.println("Enter the Number of Nodes: ");
+        n=sc.nextInt();
+
+        Node tail=null;
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the data for node"+(i+1));
+            int val=sc.nextInt();
+            Node newNode=new Node(val);
+        
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+        }
+        else{
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+    }
+    public void PrintList(){
+        if(head==null){
+            System.out.println("List is Empty:");
+            return;
+        }
+        Node current=head;
+        System.out.println("Linked List Elements is:");
+        while (current!=null) {
+            System.out.print(current.data+"-> ");
+            current=current.next;
+        }
+        System.out.println("null");
+
+    }
+    public void DeleteNode(Scanner sc){
+        Node current=head;
+        System.out.println("\nenter the position/index to delete the node: ");
+        int delete=sc.nextInt();
+
+        if(delete==0){
+            current=current.next;
+            head=current;
+        }
+        PrintList();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        DeletionSinglyLL del=new DeletionSinglyLL();
+        del.CreateList(sc);
+        del.PrintList();
+        del.DeleteNode(sc);
+    }
+}
